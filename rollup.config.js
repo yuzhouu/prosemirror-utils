f88af8448e2d8a395104a003aeeccc5ecf5841b9
coocooooo <yuzhou.ini@outlook.com>
2021-11-15 16:56:13 +0800

    refactoring build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,19 @@
-import babel from 'rollup-plugin-babel';
+import { babel } from '@rollup/plugin-babel';
 
 export default {
   input: './src/index.js',
   output: {
     format: 'cjs',
     file: 'dist/index.js',
-    sourcemap: true
+    sourcemap: true,
   },
   plugins: [
     babel({
-      exclude: 'node_modules/**'
-    })
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+    }),
   ],
   external(id) {
     return !/^[\.\/]/.test(id);
-  }
+  },
 };
