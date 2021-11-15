c2e7fcad326efa48ab4464792a6ebb066d61f078
coocooooo <yuzhou.ini@outlook.com>
2021-11-15 16:49:33 +0800

    update dep and republish as @yuzhouu/prosemirror-utils@0.0.1 for origin's 1.0.0

--- test-helpers/index.js
+++ test-helpers/index.js
@@ -3,7 +3,7 @@ import { EditorState, TextSelection, NodeSelection } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import schema from './schema';
 
-const initSelection = doc => {
+const initSelection = (doc) => {
   const { cursor, node } = doc.tag;
   if (node) {
     return new NodeSelection(doc.resolve(node));
@@ -27,24 +27,17 @@ const testHelpers = (module.exports = builders(schema, {
   em: { markType: 'em' },
   code: { markType: 'code' },
   code_block: { nodeType: 'code_block' },
-  hr: { markType: 'rule' }
+  hr: { markType: 'rule' },
 }));
 
-testHelpers.createEditor = doc => {
+testHelpers.createEditor = (doc) => {
   const place = document.body.appendChild(document.createElement('div'));
   const state = EditorState.create({
     doc,
     schema,
-    selection: initSelection(doc)
+    selection: initSelection(doc),
   });
   const view = new EditorView(place, { state });
-
-  afterEach(() => {
-    view.destroy();
-    if (place && place.parentNode) {
-      place.parentNode.removeChild(place);
-    }
-  });
 
   return { state, view, ...doc.tag };
 };
